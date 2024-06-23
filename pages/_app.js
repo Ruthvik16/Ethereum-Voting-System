4bc68b5ec3afeb7070905db43ef594582f5395cc
Aarsh <aarsh0707@gmail.com>
2024-06-24 04:22:13 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+// Import necessary modules and components
 import "@/styles/globals.css";
+import { VotingProvider } from "@/context/Voter";
+// import NavBar from "../components/NavBar/NavBar";
 
+// Export the App component as default
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <VotingProvider>
+      <div>
+        {/* <NavBar /> */}
+        <div>
+          {/* Correctly render the Component */}
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </VotingProvider>
+  );
 }
